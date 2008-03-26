2b10d9e940754fd2e351784037d546bc207b6915
Allen Short <washort@divmod.com>
2008-03-26 01:37:51 -0500

    tiny-HTML example

---
+++ examples/html.py
@@ -0,0 +1,64 @@
+"""
+A grammar for parsing a tiny HTML-like language, plus a transformer for it.
+"""
+from pymeta.grammar import OMeta
+from itertools import chain
+
+tinyHTMLGrammar = """
+
+name ::= <letterOrDigit>+:ls => ''.join(ls)
+
+tag ::= ('<' <spaces> <name>:n <spaces> <attribute>*:attrs '>'
+         <html>:c
+         '<' '/' <token n> <spaces> '>'
+             => [n.lower(), dict(attrs), c])
+
+html ::= (<text> | <tag>)*
+
+text ::= (~('<') <anything>)+:t => ''.join(t)
+
+attribute ::= <spaces> <name>:k <token '='> <quotedString>:v => (k, v)
+
+quotedString ::= (('"' | '\''):q (~<exactly q> <anything>)*:xs <exactly q>
+                     => ''.join(xs))
+
+"""
+TinyHTML = OMeta.makeGrammar(tinyHTMLGrammar, globals())
+
+def formatAttrs(attrs):
+    """
+    Format a dictionary as HTML-ish attributes.
+    """
+    return ''.join([" %s='%s'" % (k, v) for (k, v) in attrs.iteritems()])
+
+
+unparserGrammar = """
+contents ::= [<tag>*:t] => ''.join(t)
+tag ::= ([:name :attrs <contents>:t]
+            => "<%s%s>%s</%s>" % (name, formatAttrs(attrs), t, name)
+         | <anything>)
+"""
+
+TinyHTMLUnparser = OMeta.makeGrammar(unparserGrammar, globals())
+
+linkExtractorGrammar = """
+contents ::= [<tag>*:t] => list(chain(*t))
+tag ::= ( ["a" :attrs ?('href' in attrs) <contents>:t] => ([attrs['href']] + t)
+        | ["img" :attrs ?('src' in attrs) <contents>:t] => ([attrs['src']] + t)
+        | [:name :attrs <contents>:t] => t
+        | :text => [])
+"""
+
+LinkExtractor = OMeta.makeGrammar(linkExtractorGrammar, globals())
+
+boringifierGrammar = """
+contents ::= [<tag>*:t] => list(chain(*t))
+tag ::= ( ["b" <anything> <contents>:t] => t
+        | ["i" <anything> <contents>:t] => t
+        | [:name :attrs <contents>:t] => [[name, attrs, t]]
+        | :text => [text])
+"""
+
+Boringifier = OMeta.makeGrammar(boringifierGrammar, globals())
+
+testSource = "<html><title>Yes</title><body><h1>Man, HTML is <i>great</i>.</h1><p>How could you even <b>think</b> otherwise?</p><img src='HIPPO.JPG'></img><a href='http://twistedmatrix.com'>A Good Website</a></body></html>"

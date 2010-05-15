f8dbd85d8a93506e4bf89ab6653c6825f7a8574d
Allen Short <washort@divmod.com>
2010-05-15 12:05:22 -0700

    update for 0.4.0 release.

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@
 from distutils.core import setup
 setup(
     name = "PyMeta",
-    version = "0.3.0",
+    version = "0.4.0",
     url = "http://launchpad.net/pymeta",
     description = "Pattern-matching language based on OMeta",
     author = "Allen Short",
-    author_email = "washort@divmod.com",
+    author_email = "washort42@gmail.com",
     license = "MIT License",
     packages = ["pymeta"]
     )

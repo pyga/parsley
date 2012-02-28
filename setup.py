52032342fef588c79304cbb836580572ff1870df
Allen Short <washort42@gmail.com>
2012-02-27 22:18:16 -0600

    bump to 0.5

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 """Setup script for the PyMeta distribution."""
 from distutils.core import setup
 setup(
-    name = "PyMeta",
-    version = "0.4.0",
-    url = "http://launchpad.net/pymeta",
-    description = "Pattern-matching language based on OMeta",
-    author = "Allen Short",
-    author_email = "washort42@gmail.com",
-    license = "MIT License",
-    packages = ["pymeta"]
+    name="PyMeta",
+    version="0.5.0",
+    url="http://launchpad.net/pymeta",
+    description="Pattern-matching language based on OMeta",
+    long_description=open('README').read(),
+    author="Allen Short",
+    author_email="washort42@gmail.com",
+    license="MIT License",
+    packages=["pymeta"]
     )

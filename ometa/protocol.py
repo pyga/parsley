6904f2ee25ba6f61255caea3fc68367196aa98a0
Aaron Gallagher <habnabit@gmail.com>
2013-07-20 09:18:19 -0700

    Add docstrings.

--- ometa/protocol.py
+++ ometa/protocol.py
@@ -5,7 +5,23 @@ from ometa.tube import TrampolinedParser
 
 
 class ParserProtocol(Protocol):
+    """
+    A Twisted ``Protocol`` subclass for parsing stream protocols.
+    """
+
+
     def __init__(self, grammar, senderFactory, receiverFactory, bindings):
+        """
+        Initialize the parser.
+
+        :param grammar: An OMeta grammar to use for parsing.
+        :param senderFactory: A unary callable that returns a sender given a
+                              transport.
+        :param receiverFactory: A unary callable that returns a receiver given
+                                a sender.
+        :param bindings: A dict of additional globals for the grammar rules.
+        """
+
         self.grammar = grammar
         self.bindings = dict(bindings)
         self.senderFactory = senderFactory
@@ -13,6 +29,10 @@ class ParserProtocol(Protocol):
         self.disconnecting = False
 
     def connectionMade(self):
+        """
+        Start parsing, since the connection has been established.
+        """
+
         self.sender = self.senderFactory(self.transport)
         self.receiver = self.receiverFactory(self.sender)
         self.receiver.prepareParsing()
@@ -20,6 +40,12 @@ class ParserProtocol(Protocol):
             self.grammar, self.receiver, self.bindings)
 
     def dataReceived(self, data):
+        """
+        Receive and parse some data.
+
+        :param data: A ``str`` from Twisted.
+        """
+
         if self.disconnecting:
             return
 
@@ -31,6 +57,12 @@ class ParserProtocol(Protocol):
             return
 
     def connectionLost(self, reason):
+        """
+        Stop parsing, since the connection has been lost.
+
+        :param reason: A ``Failure`` instance from Twisted.
+        """
+
         if self.disconnecting:
             return
         self.receiver.finishParsing(reason)
